aef8d56bec0f77a3bc743a1589173a5c7826fa0a
Abu Sayed <rafsansayed132@gmail.com>
2022-11-26 13:58:24 +0600

    added user booking data

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ async function run(){
     try{
         const categoryCollections = client.db('ogerioDotPhone').collection('phoneCategory')
         const phoneCollections = client.db('ogerioDotPhone').collection('phoneCollection')
+        const bookingCollections = client.db('ogerioDotPhone').collection('bookingCollection')
         const usersCollections = client.db('ogerioDotPhone').collection('users')
 
         app.get('/products-category', async(req, res)=>{
@@ -30,14 +31,14 @@ async function run(){
 
         app.get('/products-category/:id', async(req, res)=>{
             const id = req.params.id;
-            const query = {_id: ObjectId(id)};
+            const query = {category_name: id};
             const products = await categoryCollections.findOne(query);
             res.send(products)
         });
 
-        app.get('/add-product/:id', async(req, res)=>{
+        app.get('/available-product/:id', async(req, res)=>{
             
-            const cursor =  phoneCollections.find({categoryId: req.params.id}).sort({"_id": -01});
+            const cursor =  phoneCollections.find({category_name: req.params.id}).sort({"_id": -01});
             const product = await cursor.toArray()
             res.send(product)
         });
@@ -65,6 +66,38 @@ async function run(){
             res.send(result)
         })
 
+        app.get('/bookings', async (req, res) => {
+            const buyerEmail = req.query.buyerEmail;
+            // const decodedEmail = req.decoded.email;
+
+            // if (email !== decodedEmail) {
+            //     return res.status(403).send({ message: 'forbidden access' });
+            // }
+
+            const query = { buyerEmail: buyerEmail };
+            const bookings = await bookingCollections.find(query).toArray();
+            res.send(bookings);
+        })
+
+        app.post('/bookings', async (req, res) => {
+            const booking = req.body;
+            // const query = {
+            //     appointmentDate: booking.appointmentDate,
+            //     email: booking.email,
+            //     treatment: booking.treatment 
+            // }
+
+            // const alreadyBooked = await bookingCollections.find(query).toArray();
+
+            // if (alreadyBooked.length){
+            //     const message = `You already have a booking on ${booking.appointmentDate}`
+            //     return res.send({acknowledged: false, message})
+            // }
+
+            const result = await bookingCollections.insertOne(booking);
+            res.send(result);
+        });
+
         app.get('/users', async (req, res) => {
             const query = {};
             const users = await usersCollections.find(query).toArray();
@@ -77,6 +110,17 @@ async function run(){
             const result = await usersCollections.insertOne(user);
             res.send(result);
         });
+        app.put('/users', async (req, res) => {
+            const user = req.body;
+            const options = { upsert: true };
+            const updatedDoc = {
+                $set: {
+                    account_type: 'Buyer Account'
+                }
+            }
+            const result = await usersCollections.updateOne(user, updatedDoc, options);
+            res.send(result);
+        })
 
 
 
